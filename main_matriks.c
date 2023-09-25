88d08d620f223cbb302d8dce2f93c1c42fb49032
MahardikaPratama <mahardika.pratama.tif422@pollban.ac.id>
2023-09-25 21:49:54 +0700

    Create File Main, Body, Header

---
+++ main_matriks.c
@@ -0,0 +1,25 @@
+/*
+Program		: main_matriks.c
+Deskripsi	: Driver / main program untuk ADT Matriks
+Dibuat oleh :
+            - Ferdi Ahmad Ariesta 	(221524041)
+            - Mahardika Pratama 	(221524044)
+            - Naia Siti Az-Zahra 	(221524052)
+            - Rafif Shabi Prasetyo  (221524055)
+            - Zahran Anugerah Rizqullah (221524063)
+Kelompok	: 2
+Kelas		: 1B
+Jurusan     : Teknik Komputer dan Informatika
+Prodi       : D4 Teknik Informatika
+Angkatan    : 2022/2023
+Tanggal		: 25 September 2023
+===============================================*/
+
+#include "matriks.h"
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    return 0;
+}
---
+++ matriks.h
@@ -0,0 +1,21 @@
+/*
+Program		: matriks.h
+Deskripsi	: Header untuk ADT Matriks
+Dibuat oleh :
+            - Ferdi Ahmad Ariesta 	(221524041)
+            - Mahardika Pratama 	(221524044)
+            - Naia Siti Az-Zahra 	(221524052)
+            - Rafif Shabi Prasetyo  (221524055)
+            - Zahran Anugerah Rizqullah (221524063)
+Kelompok	: 2
+Kelas		: 1B
+Jurusan     : Teknik Komputer dan Informatika
+Prodi       : D4 Teknik Informatika
+Angkatan    : 2022/2023
+Tanggal		: 25 September 2023
+===============================================*/
+
+#ifndef matriks_h
+#define matriks_h
+
+#endif
