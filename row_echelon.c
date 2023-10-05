fbde7d50ac66aabc64278fcd698c40a51f1c2a81
MahardikaPratama <mahardika.pratama.tif422@pollban.ac.id>
2023-10-05 17:29:59 +0700

    fixing gauss Elimination

--- row_echelon.c
+++ row_echelon.c
@@ -75,30 +75,74 @@ void MenuEselonBaris()
     return;
 }
 
-void gaussElimination(int rows, int cols, float matriks[rows][cols])
-{
-    for (int i = 0; i < rows - 1; i++)
-    {
-        for (int k = i + 1; k < rows; k++)
-        {
-            float factor = matriks[k][i] / matriks[i][i];
-            printf("Langkah %d: b%d' = b%d - (%.2f) * b%d'\n", i + 1, k + 1, k + 1, factor, i + 1);
-            for (int j = i; j < cols; j++)
-            {
-                matriks[k][j] -= factor * matriks[i][j];
+void gaussElimination(int rows, int cols, float matriks[rows][cols]) {
+    int n = rows, counter = 1;  // Jumlah persamaan
+    float *x = (float *)malloc(n * sizeof(float));
+
+    for (int i = 0; i < n - 1; i++) {
+        // Periksa apakah elemen pivot nol, dan tukar baris jika diperlukan
+        if (matriks[i][i] == 0.0) {
+            // Cari pivot non-nol di bawah baris saat ini
+            int swapRow = -1;
+            for (int k = i + 1; k < n; k++) {
+                if (matriks[k][i] != 0.0) {
+                    swapRow = k;
+                    break;
+                }
             }
+
+            if (swapRow == -1) {
+                printf("Error Matematika: Matriks singular atau ill-conditioned\n");
+                exit(0);
+            }
+
+            // Tukar baris i dan swapRow
+            for (int k = i; k < cols; k++) {
+                matriks[i][k] = -matriks[i][k];
+                float temp = matriks[i][k];
+                matriks[i][k] = matriks[swapRow][k];
+                matriks[swapRow][k] = temp;
+            }
+            
+            printf("Langkah %d: Tukar baris b%d dengan b%d\n", counter, i + 1, swapRow + 1);
+            counter++;
+            displayMatriks(n, cols, matriks);
         }
-        // Menampilkan matriks pada setiap langkah
-        printf("Matriks setelah Langkah %d:\n", i + 1);
-        displayMatriks(rows, cols, matriks);
+
+        for (int j = i + 1; j < n; j++) {
+            float ratio = matriks[j][i] / matriks[i][i];
+            printf("Langkah %d: b%d' = b%d - (%.2f) * b%d'\n", counter, j + 1, j + 1, ratio, i + 1);
+            counter++;
+            for (int k = i; k < n + 1; k++) {
+                matriks[j][k] = matriks[j][k] - ratio * matriks[i][k];
+            }
+            displayMatriks(n, cols, matriks);
+        }
     }
+
+    // Substitusi balik
+    for (int i = n - 1; i >= 0; i--) {
+        if (matriks[i][i] == 0.0) {
+            printf("Error Matematika: Matriks singular atau ill-conditioned\n");
+            exit(0);
+        }
+        x[i] = matriks[i][n];
+        for (int j = i + 1; j < n; j++) {
+            x[i] = x[i] - matriks[i][j] * x[j];
+        }
+        x[i] = x[i] / matriks[i][i];
+    }
+
+
+    free(x);
 }
+
 
 void gaussJordan(int rows, int cols, float matriks[rows][cols])
 {
     /*Kamus Data*/
     float skalar_temp;
-
+	int counter = 1;
     /*Algoritma*/
     for (int k = 0; k < rows; k++)
     {
@@ -108,7 +152,8 @@ void gaussJordan(int rows, int cols, float matriks[rows][cols])
             if (skalar_temp == 0)
                 continue; // Menghindari pembagian oleh nol
             printf("\n**************************\n");
-            printf("Langkah %d: b%d' = b%d / %.2f\n", k + 1, k + 1, k + 1, skalar_temp);
+            printf("Langkah %d: b%d' = b%d / %.2f\n", counter, k + 1, k + 1, skalar_temp);
+            counter++;
             for (int j = 0; j < cols; j++)
             {
                 matriks[k][j] = ((matriks[k][j]) / skalar_temp);
@@ -120,7 +165,8 @@ void gaussJordan(int rows, int cols, float matriks[rows][cols])
             if (i != k)
             {
                 skalar_temp = matriks[i][k];
-                printf("Langkah %d: b%d' = b%d - (%.2f) * b%d'\n", k + 1, i + 1, i + 1, skalar_temp, k + 1);
+                printf("Langkah %d: b%d' = b%d - (%.2f) * b%d'\n",counter, i + 1, i + 1, skalar_temp, k + 1);
+                counter++;
                 for (int j = 0; j < cols; j++)
                 {
                     matriks[i][j] = matriks[i][j] - (matriks[k][j] * skalar_temp);
